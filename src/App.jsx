fa7ae4618fc279261d6796a2aefa157e7463898d
SuhasK2005 <saisuhask2005@gmail.com>
2025-10-26 10:49:20 +0530

    Add Button and Hero components; update Header for smooth scrolling and dynamic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./sections/Header.jsx"
+import Hero from "./sections/Hero.jsx"
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   )
 }
