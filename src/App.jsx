72ed9dca556ac709e99e71df6bcde5c116810a54
SuhasK2005 <saisuhask2005@gmail.com>
2025-10-26 15:58:39 +0530

    Added Pricing and Faq section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import Features from "./sections/Features.jsx"
-import Header from "./sections/Header.jsx"
-import Hero from "./sections/Hero.jsx"
+import Faq from "./sections/Faq.jsx";
+import Features from "./sections/Features.jsx";
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Pricing from "./sections/Pricing.jsx";
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
       <Features />
+      <Pricing />
+      <Faq />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
